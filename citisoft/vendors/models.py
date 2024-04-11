0a1de22704dbb5195977da5e2c96dd3714902ce2
Bukunmi isijola <68000508+Izzy-drizzy@users.noreply.github.com>
2024-04-11 10:54:28 +0100

    forms authentications logic for vendor sign in/login

--- citisoft/vendors/models.py
+++ citisoft/vendors/models.py
@@ -2,24 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Vendor(models.Model):
+    username = models.CharField(max_length=100, default='unknown')
     vendor_name = models.CharField(max_length=100)
     business_name = models.CharField(max_length=100)
     vendor_desc = models.TextField()
-    contact_info = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True) 
     vendor_image = models.ImageField(upload_to='vendor_images/')
     slug = models.SlugField(max_length=30, unique=True)
 
     def __str__(self):
-        return self.vendor_name
-    
+        return self.business_name 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email_address = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
     slug = models.SlugField(max_length=30, unique=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.username    
 
 class Wishlist(models.Model):
     user = models.ForeignKey(Customer, on_delete=models.CASCADE)
--- citisoft/vendors/urls.py
+++ citisoft/vendors/urls.py
@@ -4,5 +4,9 @@ from . import views
 #URLConf
 urlpatterns = [
     path('hello', views.say_hello),
-    path('vendor_profile', views.vendor_home)
+    path('vendor_profile', views.vendor_home),
+    path('login/', views.vendor_login, name='vendor_login'),
+    path('signup/', views.vendor_signup, name='vendor_signup'),
+    path('test/', views.test_view, name='test'),
 ]
+
